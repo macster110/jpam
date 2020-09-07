153f52c013b231f2adc7f937794f6eb4ff7cc10a
au671271 <au671271@d41966.eduroam.net.au.dk>
2020-09-07 14:50:38 +0200

    First commit

---
+++ PyTorch2Java/src/main/java/DeepLearningBats/PyTorch2Java/App.java
@@ -0,0 +1,13 @@
+package DeepLearningBats.PyTorch2Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ PyTorch2Java/src/main/java/pytorch2Java/PyTorch2JavaTest.java
@@ -0,0 +1,26 @@
+package pytorch2Java;
+
+import java.util.Arrays;
+
+import org.pytorch.IValue;
+import org.pytorch.Tensor;
+import org.pytorch.Module;
+
+public class PyTorch2JavaTest {
+	
+	 public static void main(String[] args) {
+		    Module mod = Module.load("demo-model.pt1");
+		    Tensor data =
+		        Tensor.fromBlob(
+		            new int[] {1, 2, 3, 4, 5, 6}, // data
+		            new long[] {2, 3} // shape
+		            );
+		    IValue result = mod.forward(IValue.from(data), IValue.from(3.0));
+		    Tensor output = result.toTensor();
+		    System.out.println("shape: " + Arrays.toString(output.shape()));
+		    System.out.println("data: " + Arrays.toString(output.getDataAsFloatArray()));
+
+		    // Workaround for https://github.com/facebookincubator/fbjni/issues/25
+		    System.exit(0);
+		  }
+}
