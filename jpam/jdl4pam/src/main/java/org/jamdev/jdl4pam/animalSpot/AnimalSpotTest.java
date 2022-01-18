becae3b37da2f9785c849cd7b2dc57070cb1dd3c
Jamie Mac <macster110@gmail.com>
2022-01-18 13:03:30 +0000

    Create AnimalSpotTest.java

---
+++ jpam/jdl4pam/src/main/java/org/jamdev/jdl4pam/animalSpot/AnimalSpotTest.java
@@ -0,0 +1,24 @@
+package org.jamdev.jdl4pam.animalSpot;
+
+public class AnimalSpotTest {
+	
+	
+	public static void main(String[] args) {
+		String modelPath = "/Volumes/GoogleDrive-108005893101854397430/My Drive/PAMGuard_dev/Deep_Learning/AnimalSpot/Minke_spot/models/MS-all-2048/model/minke-spot.pk";
+
+		//wav file 
+		try {			
+			//first open the model and get the correct parameters. 
+			AnimalSpotModel soundSpotModel = new AnimalSpotModel(modelPath); 
+
+			//System.out.println(soundSpotModel.getRawParamsString());
+			//create the DL params. 
+			AnimalSpotParams dlParams = new AnimalSpotParams(soundSpotModel.getTransformsString());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+}
