408836a39cd7f7474ab7dfbc6a97c1ff13ec20a0
pmdnhd <32365794+pmdnhd@users.noreply.github.com>
2023-05-15 17:18:40 -0400

    Main class & some tests

--- jpam/jdl4pam/src/main/java/org/jamdev/jdl4pam/ketos/KetosModelTest.java
+++ jpam/jdl4pam/src/main/java/org/jamdev/jdl4pam/ketos/KetosModelTest.java
@@ -7,9 +7,7 @@ import java.util.ArrayList;
 import org.jamdev.jdl4pam.transforms.DLTransform;
 import org.jamdev.jdl4pam.transforms.DLTransformsFactory;
 import org.jamdev.jdl4pam.transforms.FreqTransform;
-import org.jamdev.jdl4pam.transforms.SimpleTransformParams;
 import org.jamdev.jdl4pam.transforms.WaveTransform;
-import org.jamdev.jdl4pam.transforms.DLTransform.DLTransformType;
 import org.jamdev.jdl4pam.transforms.jsonfile.DLTransformsParser;
 import org.jamdev.jdl4pam.utils.DLMatFile;
 import org.jamdev.jdl4pam.utils.DLUtils;
@@ -33,7 +31,7 @@ public class KetosModelTest  {
 //		File file = new File("/Users/au671271/Library/CloudStorage/GoogleDrive-macster110@gmail.com/My Drive/PAMGuard_dev/Deep_Learning/Ketos/narw_2/kw_detector_v11_5s_test/kw_detector_v11_5s.ktpb");
 
 		//a new classifier based on a different architecture
-		File file = new File("/Users/au671271/Library/CloudStorage/GoogleDrive-macster110@gmail.com/My Drive/PAMGuard_dev/Deep_Learning/Ketos/narw_2/hallo-kw-det_v1_test/hallo-kw-det_v1.ktpb"); 
+		File file = new File("/home/postdoc/PycharmProjects/ActiveLearning/EastPtHydro/hallo-kw-det_v1_test/hallo-kw-det_v1.ktpb");
 
 		/****Wav files*****/
 
@@ -45,120 +43,99 @@ public class KetosModelTest  {
 //		String wavFilePath = "/Users/au671271/Library/CloudStorage/GoogleDrive-macster110@gmail.com/My Drive/PAMGuard_dev/Deep_Learning/Ketos/narw_2/JASCO_resampled_20k.wav";
 		
 		//jasco_reduced - use for right whales hallo-kw-det_v1
-		String wavFilePath = "/Users/au671271/Library/CloudStorage/GoogleDrive-macster110@gmail.com/My Drive/PAMGuard_dev/Deep_Learning/Ketos/narw_2/hallo-kw-det_v1_test/audio/jasco_reduced.wav";
-		double[] timelims = new double[] {0., 5.0}; //5 second window
-		
-		
-		/****Output MAT file for diagnosis****/
-		String outputMatfile = "/Users/au671271/Library/CloudStorage/GoogleDrive-macster110@gmail.com/My Drive/Programming/MATLAB/PAMGUARD/deep_learning/ketos_classifier/java_transformed.mat"; 
+		String wavFilePath = "/home/postdoc/PycharmProjects/ActiveLearning/EastPtHydro/hallo-kw-det_v1_test/audio/jasco_reduced.wav";
 
 		try {
-			//the ketos model. 
-			KetosModel  ketosModel = new KetosModel(file);
+			double audioDuration = DLUtils.loadWavFile(wavFilePath).getLengthInSeconds();
+			// seglen in ketosParams
+			double segLength = 5.; // [s]
+			// Step between time windows. Not the step size in the FFT for spectro computation
+			double stepSeg = 5.; // [s]
+			// class FrameStepper(SelectionGenerator) __init__ method in audio_loader.py
+			// Always consider padding at the end if needed to get the whole audio processed. If pad: else: in __init__
+			int numTimeSegs = (int) Math.ceil((audioDuration - segLength) / stepSeg) + 1;
 
-			//read the JSOn string from the the file. 
-			String jsonString  = DLTransformsParser.readJSONString(new File(ketosModel.getAudioReprFile()));
+			for (int timewind= 0; timewind<numTimeSegs; timewind++) {
 
-			//get the audio representation file. 
-			KetosParams ketosParams = new KetosParams(jsonString); 			
+				/****Output MAT file for diagnosis****/
+				String outputMatfile = "/home/postdoc/PycharmProjects/ActiveLearning/EastPtHydro/hallo-kw-det_v1_test/output_spectro/java_transformed_dell" + timewind + ".mat";
 
-			System.out.println(ketosParams.toString());
+				//the ketos model.
+				KetosModel ketosModel = new KetosModel(file);
 
-			//Open wav files. 
-			AudioData soundData = DLUtils.loadWavFile(wavFilePath);
-			soundData = soundData.trim((int) (soundData.getSampleRate()*timelims[0]), (int) (soundData.getSampleRate()*timelims[1])); 
+				//read the JSOn string from the file.
+				String jsonString = DLTransformsParser.readJSONString(new File(ketosModel.getAudioReprFile()));
 
-			//generate the transforms. 
-			
-			ArrayList<DLTransform> transforms =	DLTransformsFactory.makeDLTransforms(ketosParams.dlTransforms); 
-						
-			ketosModel.setInputShape(ketosParams.defaultInputShape);
+				//get the audio representation file.
+				KetosParams ketosParams = new KetosParams(jsonString, timewind * segLength);
 
-			((WaveTransform) transforms.get(0)).setWaveData(soundData); 
-
-			Matrix transfromedDataM =null; 
-
-			DLTransform transform = transforms.get(0); 
-			for (int i=0; i<transforms.size(); i++) {
-				System.out.println(transforms.get(i).getDLTransformType()); 
-				transform = transforms.get(i).transformData(transform); 
-				//				if (i==1) {
-				//					 transfromedData =  DLMatFile.array2Matrix(((FreqTransform) transform).getSpecTransfrom().getTransformedData());
-				//				}
-				//				if (transforms.get(i) instanceof FreqTransform) {
-				//				transformedData = ((FreqTransform) transform).getSpecTransfrom().getTransformedData(); 
-				//					System.out.println("Transform shape: " + i + " " +  transformedData.length + " " + transformedData[0].length); 
-				//				}
-			}
-
-			double[][] transformedData = ((FreqTransform) transform).getSpecTransfrom().getTransformedData();
-			
-			System.out.println("Image size: " + transformedData.length + " x " + transformedData[0].length);  
-
-			transfromedDataM =  DLMatFile.array2Matrix(transformedData);
-			MatFile matFile = Mat5.newMatFile()
-					.addArray("transformed_data_j", transfromedDataM);
-			try {
-				Mat5.writeToFile(matFile, outputMatfile);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-//			/*****Import the transformed data from Python and use that instead*****/
-
-//			Matrix trueTransformedDataM = Mat5.readFromFile("/Users/au671271/MATLAB-Drive/MATLAB/PAMGUARD/deep_learning/ketos_classifier/pythontransfored4java.mat")
-//					.getMatrix("transformed_data_py");
-			
-//			Matrix trueTransformedDataM = Mat5.readFromFile("/Users/au671271/MATLAB-Drive/MATLAB/PAMGUARD/deep_learning/ketos_classifier/pythontransfored4java_hallo-kw-det_v1.mat").
-//					getMatrix("transformed_data_py");
-//			
-//			double[][] trueTransformedData = new double[trueTransformedDataM.getDimensions()[0]][trueTransformedDataM.getDimensions()[1]]; 
-//			for (int i=0; i<trueTransformedDataM.getDimensions()[0]; i++) {
-//				for (int j=0; j<trueTransformedDataM.getDimensions()[1]; j++) {
-//					trueTransformedData[i][j]=  trueTransformedDataM.getDouble(i, j);
-//				}
-//			}
-//			transformedData = trueTransformedData;
-			
-			/*********************************************************************/
-
-
-			float[] output = null; 
-			float[][][] data;
-			int nStack = 1; //number of specs to give to the classifier. 
-			for (int i=0; i<10; i++) {
-				long time1 = System.currentTimeMillis();
-				data = new float[nStack][][]; 
-				for (int j=0; j<nStack; j++) {
-					data[j] = DLUtils.toFloatArray(transformedData); 
+				double[] offsets = ketosParams.computeOffsets(jsonString);
+				double startTime = 0.;
+				if (offsets[2] < 0){
+					startTime = 0.;
 				}
-				output = ketosModel.runModel(data); 
-				long time2 = System.currentTimeMillis();
-				System.out.println("Time to run model: " + (time2-time1) + " ms"); 
+				else {
+					startTime = offsets[2];
+				}
+
+				// Time limits of the audio would be after the new duration computed in the KetosParams function
+				double[] timelims = new double[]{startTime, timewind * segLength + ketosParams.seglen}; //5 second window
+
+				System.out.println(ketosParams.toString());
+
+				//Open wav files.
+				AudioData soundDataWind = DLUtils.loadWavFile(wavFilePath);
+				soundDataWind = soundDataWind.trim((int) (DLUtils.numSamplesKetos(startTime,
+						soundDataWind.sampleRate, Boolean.FALSE)), (int) (DLUtils.numSamplesKetos(timelims[1], soundDataWind.sampleRate, Boolean.FALSE)));
+
+				//generate the transforms.
+				ArrayList<DLTransform> transforms = DLTransformsFactory.makeDLTransforms(ketosParams.dlTransforms);
+
+				ketosModel.setInputShape(ketosParams.defaultInputShape);
+				//			FreqTransform a = new FreqTransform(new DLTransform.DLTransformType("SPECTROGRAMKETOS"), transforms.get(2).getDLTransformType())
+
+				((WaveTransform) transforms.get(0)).setWaveData(soundDataWind);
+
+				Matrix transfromedDataM = null;
+
+				DLTransform transform = transforms.get(0);
+				for (int i = 0; i < transforms.size(); i++) {
+					System.out.println(transforms.get(i).getDLTransformType());
+					//				transform = transforms.get(i).transformData(transform);
+					if (transforms.get(i).getDLTransformType().name().equals("SPECTROGRAMKETOS")) {
+						transform = transforms.get(i).transformData(transform);
+					} else {
+						transform = transforms.get(i).transformData(transform);
+						int num_pad_right = 0;
+						if (transforms.get(i).getDLTransformType().name().equals("DECIMATE")) {
+							// Apply padding after resampling cf Ketos
+							num_pad_right = Math.max(0, (int) (((timelims[1] - startTime) * soundDataWind.sampleRate - soundDataWind.samples.length)));
+							soundDataWind.appendLeftRight((int) offsets[3], num_pad_right);
+						}
+					}
+				}
+
+				double[][] transformedData = ((FreqTransform) transform).getSpecTransfrom().getTransformedData();
+
+				System.out.println("Image size: " + transformedData.length + " x " + transformedData[0].length);
+
+				transfromedDataM = DLMatFile.array2Matrix(transformedData);
+				MatFile matFile = Mat5.newMatFile()
+						.addArray("transformed_data_j", transfromedDataM);
+				try {
+					Mat5.writeToFile(matFile, outputMatfile);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-
-
-
-			double[] prob = new double[output.length]; 
-			for (int j=0; j<output.length; j++) {
-				//python code for this. 
-				//		    	prob = torch.nn.functional.softmax(out).numpy()[n, 1]
-				//	                    pred = int(prob >= ARGS.threshold)		    	
-				//softmax function
-				System.out.println("The output is: " + output[j]); 
-
-				
-				prob[j] = DLUtils.softmax(output[j], output); 
-				//System.out.println("The probability is: " + prob[j]); 
-			}
-
 
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
+		System.exit(0);
 	}
 
 
