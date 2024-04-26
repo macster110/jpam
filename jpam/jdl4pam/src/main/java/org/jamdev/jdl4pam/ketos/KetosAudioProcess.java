5e6e75378f27a158d234f4804a832f0d8f8c46c5
Jamie Mac <macster110@gmail.com>
2024-04-26 16:04:24 +0100

    Update version numbers

--- jpam/jdl4pam/src/main/java/org/jamdev/jdl4pam/ketos/KetosAudioProcess.java
+++ jpam/jdl4pam/src/main/java/org/jamdev/jdl4pam/ketos/KetosAudioProcess.java
@@ -212,7 +212,6 @@ public class KetosAudioProcess {
 			if (verbosity>0) System.out.println(" ");  
 		
 
-
 			ketosResults.add(new KetosResult(prob, timelims[0], ketosParams.segLen)); 	
 
 			/**************************************************************/
